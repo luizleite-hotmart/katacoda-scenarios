223346492cd041d1219429d216851eebf28dbd70
luizoliveira <xp.luiz@gmail.com>
2017-04-11 22:49:21 -0300

    return to curl

--- clojure-initial/cmd1.sh
+++ clojure-initial/cmd1.sh
@@ -1,3 +1,5 @@
-curl /usr/bin/lein \
-    https://raw.githubusercontent.com/technomancy/leiningen/stable/bin/lein \
-    && chmod +x /usr/bin/lein
+curl "https://raw.githubusercontent.com/technomancy/leiningen/stable/bin/lein" > lein.txt
+mkdir -p /usr/local/bin/
+mv /home/scrapbook/tutorial/lein* /usr/local/bin/lein
+chmod a+x /usr/local/bin/lein
+export PATH=$PATH:/usr/local/bin
