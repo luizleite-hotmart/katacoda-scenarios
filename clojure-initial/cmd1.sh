3d367994beb8b0ac17a0d6caf7045fbc8876c723
luizoliveira <xp.luiz@gmail.com>
2017-04-11 22:35:32 -0300

    change the files

--- clojure-initial/cmd1.sh
+++ clojure-initial/cmd1.sh
@@ -1,5 +1,5 @@
 curl "https://raw.githubusercontent.com/technomancy/leiningen/stable/bin/lein" > lein.txt
 mkdir -p /usr/local/bin/
-mv ~/lein* /usr/local/bin/lein
+mv /home/scrapbook/tutorial/lein* /usr/local/bin/lein
 chmod a+x /usr/local/bin/lein
 export PATH=$PATH:/usr/local/bin
