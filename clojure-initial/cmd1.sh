b24d0802ef5098578dab15176e769a327bb37d97
luizleiteoliveira <luiz.leite@hotmart.com.br>
2017-04-17 21:23:01 -0300

    remove commands from cmd

--- clojure-initial/cmd1.sh
+++
@@ -1,5 +0,0 @@
-git clone https://github.com/pandeiro/docker-lein && cd docker-lein
-docker build -t 'lein' .
-docker pull pandeiro/lein
-alias clj-repl='docker run -i -t pandeiro/lein repl'
-clj-repl
